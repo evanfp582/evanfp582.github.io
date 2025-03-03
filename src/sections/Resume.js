3b2d141abf6b303fecdda3bf7aabc40a838d2600
evanfp582 <evanfp582@gmail.com>
2025-03-03 10:42:24 -0500

    Added resume and resume section

--- src/sections/Resume.js
+++ src/sections/Resume.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button, Link } from "@mui/material";
 
 const Resume = () => {
   return (
@@ -7,20 +7,42 @@ const Resume = () => {
       id="resume"
       sx={{
         minHeight: "100vh",
-        backgroundColor: "background.secondary",
+        backgroundColor: "background.default",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        flexDirection: "column",
+        textAlign: "center",
       }}
     >
-      <Box textAlign="center">
-        <Typography variant="h2" color="primary" sx={{ fontWeight: "bold" }}>
-          My Resume
-        </Typography>
-        <Typography variant="h4" color="text.secondary" sx={{ mt: 2 }}>
-          Details about my experience and skills
-        </Typography>
+      <Typography variant="h2" color="primary" sx={{ fontWeight: "bold", mb: 4 }}>
+        My Resume!
+      </Typography>
+
+      <Box sx={{ width: "50%", height: "80vh", mb: 2, display: "flex", justifyContent: "center", }}>
+        <embed
+          src="/images/EvanFisherPerezResumeV1.pdf"
+          type="application/pdf"
+          width="100%"
+          height="100%"
+        />
       </Box>
+
+      <Link
+        href="/images/EvanFisherPerezResumeV1.pdf"
+        download="EvanFisherPerez_Resume.pdf"
+        sx={{
+          textDecoration: "none",
+          color: "secondary.main",
+          fontSize: "1.2rem",
+          display: "inline-block",
+          mb: 4,
+        }}
+      >
+        <Button variant="contained" color="secondary">
+          Download Resume
+        </Button>
+      </Link>
     </Box>
   );
 };
